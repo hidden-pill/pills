ed333da916db175f1765595d65c44a44593f4ff9
egalland <emmanuel.galland117@gmail.com>
2018-10-05 15:27:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation Starter Template</title>
+    <link rel="stylesheet" href="assets/css/foundation.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+<?php include ''; ?>
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/what-input.js"></script>
+    <script src="assets/js/vendor/foundation.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    <script src="assets/js/script.js"></script>
+  </body>
+</html>
