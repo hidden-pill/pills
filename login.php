e806f4e2a9f9c5410777aae97ffd91cb589a9795
egalland <emmanuel.galland117@gmail.com>
2018-10-15 16:29:35 +0200

    .

--- login.php
+++ login.php
@@ -1,14 +1,16 @@
 <div class="reveal" id="logIn" data-reveal>
     <h1>Connexion</h1>
     <label class="" for="pseudo">pseudo</label>
-    <input id="pseudo" class="" name="pseudo" placeholder="Pseudo" type="text" value="" />
+    <input id="pseudo" class="" name="pseudo" placeholder="Pseudo" type="text" />
     <label class="" for="password">password</label>
-    <input id="password" class="" name="password" placeholder="password" type="text" value="" />
+    <input id="password" class="" name="password" placeholder="password" type="password" />
     <label class="" for="keepMeLogIn">keepMeLogIn</label>
     <p><input id="keepMeLogIn" class="" name="keepMeLogIn" type="checkbox" /> keepMeLogIn</p>
     <input id="submitLogin" class="button" name="submitLogin" type="submit" value="Se connecter" />
-    <input type="button" data-close aria-label="Close modal" value="Annuler" />
-    <a href="register.php">S'inscrire</a>
+    <input class="button secondary" type="button" data-close aria-label="Close modal" value="Annuler" />
+    <div class="register">
+        <a class="button" href="register.php">Créer un compte</a>
+    </div>
     <button class="close-button" data-close aria-label="Close modal" type="button">
         <span aria-hidden="true">&times;</span>
     </button>
