e6c26e462a770594bb45eef3c46bd4b8295d817f
egalland <emmanuel.galland117@gmail.com>
2018-11-01 22:21:14 +0100

    1/11

--- configuration.php
+++ configuration.php
@@ -7,6 +7,11 @@ define('CHARSET', 'utf-8');
 define('LOGIN', '');
 define('PASSWORD', '');
 
+
+//salt
+define('SALT', '');
+
+
 // Ajout des fichiers nécessaire au bon fonctionnement du site
 include_once 'class/database.php';
-//include_once 'models/users.php';
+include_once 'models/users.php';
--- models/users.php
+++ models/users.php
@@ -15,4 +15,47 @@ class Users extends Database {
     public function __constructor() {
         parent::__construct();
     }
+
+        public function userConnection() {
+        $state = false;
+        $query = 'SELECT `pseudo`, `password`, `image` FROM `' .SALT. 'users` WHERE `pseudo` = :identifier OR `email` = :identifier';
+        $result = $this->db->prepare($query);
+        $result->bindValue(':identifier', $this->identifier, PDO::PARAM_STR);
+        if ($result->execute()) { //On vérifie que la requête s'est bien exécutée
+            $selectResult = $result->fetch(PDO::FETCH_OBJ);
+            if (is_object($selectResult)) { //On vérifie que l'on a bien trouvé un utilisateur
+                // On hydrate
+                $this->pseudo = $selectResult->pseudo;
+                $this->password = $selectResult->password;
+                $this->image = $selectResult->image;
+                $state = true;
+            }
+        }
+        return $state;
+    }
+
+        public function userRegister() {
+        $query = '';
+        $result = $this->db->prepare($query);
+        $result->bindValue(':pseudo', $this->pseudo, PDO::PARAM_STR);
+        $result->bindValue(':password', $this->password, PDO::PARAM_STR);
+        $result->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $result->bindValue(':secretQuestion', $this->secretQuestion, PDO::PARAM_INT);
+        $result->bindValue(':secretAnswer', $this->secretAnswer, PDO::PARAM_STR);
+        $result->bindValue(':newsletter', $this->newsletter, PDO::PARAM_INT);
+        $result->bindValue(':birthDate', $this->birthDate, PDO::PARAM_STR);
+        return $result->execute();
+    }
+
+    public function checkIfUserExist(){
+        $state = false;
+        $query = 'SELECT COUNT(`id`) AS `count` FROM `users` WHERE `pseudo` = :pseudo';
+        $result = $this->db->prepare($query);
+        $result->bindValue(':pseudo', $this->login, PDO::PARAM_STR);
+        if ($result->execute()) {
+            $selectResult = $result->fetch(PDO::FETCH_OBJ);
+            $state = $selectResult->count;
+        }
+        return $state;
+    }
 }
