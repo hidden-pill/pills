bc1ff77fb52d92ccb662df09bcf85110ebd8d990
Emmanuel Galland <emmanuel.galland117@gmail.com>
2018-11-12 18:48:19 +0100

    .

---
+++ controllers/addArtistController.php
@@ -0,0 +1,4 @@
+<?php
+if(isset($_POST['submitArtist'])){
+    var_dump($_POST);
+}
--- views/addArtist.php
+++ views/addArtist.php
@@ -2,5 +2,16 @@
 include_once 'header.php';
 include_once '../controllers/addArtistController.php';
 ?>
-
+<form action="addArtist.php" method="POST" enctype="multipart/form-data">
+<div class="input-field">
+    <select multiple name="select[]">
+        <option disabled selected>Choose your option</option>
+        <option value="aaaaa">Option 1</option>
+        <option value="bbbbb">Option 2</option>
+        <option value="ccccc">Option 3</option>
+        </select>
+        <label>Materialize Multiple Select</label>
+    </div>
+<input type="submit" name="submitArtist" />
+</form>
 <?php include_once 'footer.php'; ?>
