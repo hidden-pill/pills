06f120870a1db568c26e6d148e35b8cc84cfbb30
Emmanuel Galland <emmanuel.galland117@gmail.com>
2018-11-23 17:05:19 +0100

    add reward score

--- ajax/score.php
+++ ajax/score.php
@@ -4,6 +4,9 @@ if(isset($_SESSION['id'])){
     $id = $_SESSION['id'];
 
     if(isset($_POST['score']) && !empty($_POST['id_column']) && !empty($_POST['column'])){
+        $rewardUser = new Users();
+        $rewardUser->id = $id;
+        $rewardUser->rewardID = 1;
         $score = new Scores();
         $score->id_users = $id;
         $score->id_column = $_POST['id_column'];
@@ -12,8 +15,12 @@ if(isset($_SESSION['id'])){
         if($score->checkIfScoreExist($column)){
             if($score->selectScore($column) == $_POST['score']){
                 if($score->deleteScore($column)){
-                    $jsonReturn = ['sum' => $score->selectTotalScore($column), 'action' => 'del', 'button' => $_POST['score'], 'item' => $_POST['id_column'], 'column' => $column];
-                    echo json_encode($jsonReturn);
+                    if($rewardUser->deleteUserReward()){
+                        $jsonReturn = ['sum' => $score->selectTotalScore($column), 'action' => 'del', 'button' => $_POST['score'], 'item' => $_POST['id_column'], 'column' => $column];
+                        echo json_encode($jsonReturn);
+                    }else {
+                        echo json_encode(['error' => 'error']);
+                    }
                 }else{
                     echo json_encode(['error' => 'error']);
                 }
@@ -27,8 +34,12 @@ if(isset($_SESSION['id'])){
             }
         }else{
             if($score->insertScore($column)){
-                $jsonReturn = ['sum' => $score->selectTotalScore($column), 'action' => 'ins', 'button' => $_POST['score'], 'item' => $_POST['id_column'], 'column' => $column];
-                echo json_encode($jsonReturn);
+                if($rewardUser->addUserReward()){
+                    $jsonReturn = ['sum' => $score->selectTotalScore($column), 'action' => 'ins', 'button' => $_POST['score'], 'item' => $_POST['id_column'], 'column' => $column];
+                    echo json_encode($jsonReturn);
+                }else {
+                    echo json_encode(['error' => 'error']);
+                }
             } else {
                 echo json_encode(['error' => 'error']);
             }
